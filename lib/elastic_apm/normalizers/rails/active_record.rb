7406f77a573ea28fb54a4309888882b38a2c17f1
Emily Stolfo <emstolfo@gmail.com>
2019-11-06 16:12:43 +0100

    Revert "Lazy load ActiveRecord adapter in SQL normalizer (#585)"
    This reverts commit 0245a39ef7313e298874590ef1352f2d02a6d821.

--- lib/elastic_apm/normalizers/rails/active_record.rb
+++ lib/elastic_apm/normalizers/rails/active_record.rb
@@ -15,6 +15,7 @@ module ElasticAPM
         def initialize(*args)
           super
 
+          @subtype = lookup_adapter || 'unknown'
           @summarizer = SqlSummarizer.new
         end
 
@@ -24,14 +25,10 @@ module ElasticAPM
           name = summarize(payload[:sql]) || payload[:name]
           context =
             Span::Context.new(db: { statement: payload[:sql], type: 'sql' })
-          [name, TYPE, subtype, ACTION, context]
+          [name, TYPE, @subtype, ACTION, context]
         end
 
         private
-
-        def subtype
-          @subtype ||= (lookup_adapter || 'unknown')
-        end
 
         def summarize(sql)
           @summarizer.summarize(sql)
