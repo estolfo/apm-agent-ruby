6a8769fabf983473f43589021ab6bce51c0be239
Emily Stolfo <emily.stolfo@elastic.co>
2021-09-13 14:38:11 +0200

    Try posting differently

--- lib/elastic_apm.rb
+++ lib/elastic_apm.rb
@@ -409,12 +409,12 @@ module ElasticAPM
     end
 
     def flush_data
-      @client = HTTP.headers('Content-Type' => 'application/x-ndjson')
       url = "#{agent&.config.data_collector_url}/intake/v2/events"
-      puts "flushing data to url #{url}"
       json = concatenated_json
+      client = HTTP.headers({ 'Content-type' => 'application/x-ndjson' })
+      puts "flushing data to url #{url}"
       puts "data is #{json}"
-      resp = @client.post(url, body: concatenated_json)
+      resp = client.post(url, body: json)
       puts "resp from post #{resp}"
     rescue => e
       puts "Got error when posting: #{e}"
